c6ba15d2937109800fcb38d86a76c1755a53f258
dealrachaan <33548086+dealrachaan@users.noreply.github.com>
2017-12-20 03:06:56 +0000

    Update UserInformation.java

--- src/test/java/org/kohsuke/github/UserInformation.java
+++ src/test/java/org/kohsuke/github/UserInformation.java
@@ -1,13 +1,5 @@
 package org.kohsuke.github;
 
-import commands.Command;
-import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.entities.Role;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import sun.security.x509.AVA;
-import utils.STATICS;
-
 import java.awt.*;
 import java.io.IOException;
 import java.text.ParseException;
@@ -26,9 +18,11 @@ import java.util.TreeMap;
 
 public class UserInformation implements GHMyself {
 
-    @Override
-    public boolean called(String[] args, MessageReceivedEvent event) {
-        return false;
-    }
 
-   
+    GHUser user = new GHUser();
+    List<String> userEmails = new List<String>();
+    List<GHEmail> userEmails2 = new List<GHEmail>;
+    List<GHKey> userKeys = new List<UserKeys>;
+    
+
+       
