9406c6a6f4d0bc7edb3fbe5f1cf2cc3aecee1145
dealrachaan <33548086+dealrachaan@users.noreply.github.com>
2017-12-20 03:36:41 +0000

    Update UserInformation.java

--- src/test/java/org/kohsuke/github/UserInformation.java
+++ src/test/java/org/kohsuke/github/UserInformation.java
@@ -1,7 +1,6 @@
 package org.kohsuke.github;
 
 import java.awt.*;
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
@@ -14,19 +13,29 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-
+import com.infradna.tool.bridge_method_injector.WithBridgeMethods;
+import java.io.IOException;
 
 public class UserInformation implements GHMyself {
 
-
+    //from GHMyself.java
     GHUser user = new GHUser();
     List<String> userEmails = new List<String>();
-    List<GHEmail> userEmails2 = new List<GHEmail>;
-    List<GHKey> userKeys = new List<UserKeys>;
-    List<GHVerifiedKey> userVerifiedKeys = new List<GHVerifiedKey>;
-    GHPersonSet<GHOrganization> userOrganisations = new GHPersonSet<GHOrganization>;
-    Map<String,GHRepository> userRepositories = new Map<String,GHRepository>;
-    PagedIterable<GHRepository> userRepositoryList = new PagedIterable<GHRepository>;
-    PagedIterable<GHMembership> userOrgMemberships = new PagedIterable<GHMembership>;
+    List<GHEmail> userEmails2 = new List<GHEmail>();
+    List<GHKey> userKeys = new List<UserKeys>();
+    List<GHVerifiedKey> userVerifiedKeys = new List<GHVerifiedKey>();
+    GHPersonSet<GHOrganization> userOrganizations = new GHPersonSet<GHOrganization>();
+    Map<String,GHRepository> userOwnedRepositories = new Map<String,GHRepository>();
+    PagedIterable<GHRepository> userOwnedRepositoryList = new PagedIterable<GHRepository>();
+    PagedIterable<GHMembership> userOrgMemberships = new PagedIterable<GHMembership>();
     
-    
+    //fromGHUser.java
+    PagedIterable<GHUser> userFollowingList = new PagedIterable<GHUser>();
+    GHPersonSet<GHUser> userFollowersList = new GHPersonSet<GHUser>();
+    PagedIterable<GHRepository> userFollowedRepositories = new PagedIterable<GHRepository>();
+    PagedIterable<GHRepository> userStarredRepositories = new PagedIterable<GHRepository>();
+    boolean userIsOrgMember(GHOrganization);
+    boolean userIsTeamMember(GHTeam);
+    boolean userIsPublicMemberOf(GHOrganization);
+    PagedIterable<GHEventInfo> userEvents = new PagedIterable<GHEventInfo>();
+    PagedIterable<GHGist> userGists = new PagedIterable<GHGist> listGists();
