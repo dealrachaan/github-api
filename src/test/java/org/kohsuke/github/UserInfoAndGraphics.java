a9f854fe92da16e4e6b7bab57b5b5eaaaf298ae1
dealrachaan <33548086+dealrachaan@users.noreply.github.com>
2017-12-20 22:09:29 +0000

    Update UserInfoAndGraphics.java

--- src/test/java/org/kohsuke/github/UserInfoAndGraphics.java
+++ src/test/java/org/kohsuke/github/UserInfoAndGraphics.java
@@ -15,7 +15,38 @@ import java.net.URL;
 import java.util.Date;
 
 
-public class UserInfoAndGraphics{
+public class UserInfoAndGraphics(UserInformation myself){
+     
+     GHPersonSet<GHOrganization> allOrgs = new GHPersonSet<GHOrganization>();
+     PagedIterable<GHUser> allMembers = new PagedIterable<GHUser>;
+     
+     public void addUserOrganizations(GHUser user){
+          GHPersonSet<GHOrganization> thisUsersOrgs = user.getOrgs();
+          for (GHOrganization org : thisUsersOrgs) {
+               boolean newOrg = allOrgs.add(org);
+               if(newOrg){
+                    //if this is a newly introduced org, create new visual node for this org
+                    //add this orgs users to the graph
+                    addOrgUsers(org);
+               }else{
+                    //if not, link it to existing member nodes of this org
+               }
+          }
+     }
+     
+     public void addOrgUsers(GHOrganization org){
+          PagedIterable<GHUser> thisOrgsUsers = org.getMembers();
+          for (GHUser user : thisOrgsUsers) {
+               boolean newUser = allOrgs.add(org);
+               if(newUser){
+                    //if this is a newly introduced org, create new visual node for this org
+                    //add this users orgs to the graph
+                    addUserOrganizations(user);
+               }else{
+                    //if not, link it to existing user nodes for this member
+               }
+          }
+     }
 }
 
 public static void main (String args[]){
