b193381409fa762ed787a38fa54e6df9865fb2f1
dealrachaan <33548086+dealrachaan@users.noreply.github.com>
2017-12-20 19:22:22 +0000

    Create UserInfoAndGraphics.java

---
+++ src/test/java/org/kohsuke/github/UserInfoAndGraphics.java
@@ -0,0 +1,20 @@
+package org.kohsuke.github;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import com.infradna.tool.bridge_method_injector.WithBridgeMethods;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Date;
+
+public static void main (String args[]){
+     UserInformation myself = new UserInformation();
+     printAll(myself);
+}
